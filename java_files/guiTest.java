1405db8f56f6f916ce42431fbbbf329e7b93a9b7
radzsza <aleksandraradziukiewicz@gmail.com>
2025-05-17 15:04:29 +0200

    i am still learning, ok?

--- java_files/guiTest.java
+++ java_files/guiTest.java
@@ -1,13 +1,51 @@
 package java_files;
+//Usually you will require both swing and awt packages
+// even if you are working with just swings.
 import javax.swing.*;
+import java.awt.*;
+class guiTest {
+    public static void main(String args[]) {
 
-public class guiTest {
-    public static void main(String args[]){
-        JFrame frame = new JFrame("My First GUI");
+        //Creating the Frame
+        JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setSize(600, 600);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        reset.addActionListener(e -> {tf.setText("");});
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        send.addActionListener(e -> {
+            ta.append(tf.getText());
+            ta.append("\n");
+            tf.setText("");
+        });
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
     }
 }
