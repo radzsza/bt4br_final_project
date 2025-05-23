369b6729aa4cb9643cf194cc7746132e86e6d119
radzsza <aleksandraradziukiewicz@gmail.com>
2025-05-24 01:06:20 +0200

    main menu project in java swing style

--- java_files/TestGUI.java
+++ java_files/TestGUI.java
@@ -1,8 +1,11 @@
 // nawet na to nie patrz, sama średnio wiem co tu się dzieje
 package java_files;
 
+import javax.imageio.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.*;
 
 public class TestGUI {
 
@@ -15,12 +18,13 @@ public class TestGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("TestGUI");
         frame.setPreferredSize(new Dimension(800, 600));
-        frame.add(new TestMainPanel());
+        frame.add(new TestMainPanel(1));
         frame.pack();
         frame.setVisible(true);
     }
 
     class TestMainPanel extends JPanel {
+
         public TestMainPanel() {
             setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
@@ -40,6 +44,31 @@ public class TestGUI {
             c.gridy = 3;
             c.weighty = 10;
             JButton button2 = new JButton("Option 2");
+            add(button2, c);
+        }
+
+        public TestMainPanel(int x) {
+            setLayout(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+
+            c.gridx = 1;
+            c.gridy = 0;
+            c.gridwidth = 3;
+            c.weighty = 1;
+            c.ipady = 150;
+            c.ipadx = 300;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            JButton button = new JButton("START");
+            button.setFont(new Font("Monospaced", Font.PLAIN, 50));
+            add(button, c);
+
+            c.gridx = 3;
+            c.gridy = 1;
+            c.weighty = 1;
+            c.ipady = 20;
+            c.ipadx = 0;
+            JButton button2 = new JButton("Info");
+            button2.setFont(new Font("Monospaced", Font.PLAIN, 25));
             add(button2, c);
         }
     }
