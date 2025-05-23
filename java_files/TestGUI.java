0d363c2f5fd4ee0936e08f71191361db73d20498
radzsza <aleksandraradziukiewicz@gmail.com>
2025-05-23 18:20:29 +0200

    mainly TestGUI - i try to make main menu

--- java_files/TestGUI.java
+++ java_files/TestGUI.java
@@ -1,4 +1,46 @@
+// nawet na to nie patrz, sama średnio wiem co tu się dzieje
 package java_files;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TestGUI {
+
+    public static void main(String[] args) {
+        new TestGUI();
+    }
+
+    public TestGUI() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("TestGUI");
+        frame.setPreferredSize(new Dimension(800, 600));
+        frame.add(new TestMainPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    class TestMainPanel extends JPanel {
+        public TestMainPanel() {
+            setLayout(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+            //c.fill = GridBagConstraints.BOTH;
+            c.gridx = 2;
+            c.gridy = 1;
+            c.weighty = 10;
+            c.gridwidth = 3;
+            c.ipady = 100;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            //c.anchor = GridBagConstraints.CENTER;
+            JButton button = new JButton("Option 1");
+            add(button, c);
+            //c.anchor = GridBagConstraints.CENTER;
+
+            c.gridx = 2;
+            c.gridy = 3;
+            c.weighty = 10;
+            JButton button2 = new JButton("Option 2");
+            add(button2, c);
+        }
+    }
 }
