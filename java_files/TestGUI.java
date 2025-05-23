34f28f6f5789ba8b1581b6af41f0ac3e708b31cd
radzsza <aleksandraradziukiewicz@gmail.com>
2025-05-24 01:18:23 +0200

    quick graphics tests

--- java_files/TestGUI.java
+++ java_files/TestGUI.java
@@ -1,26 +1,47 @@
 // nawet na to nie patrz, sama średnio wiem co tu się dzieje
+// używanie obrazka jeszcze jest do dopracowania (będę musiała zrobić layering)
 package java_files;
 
 import javax.imageio.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class TestGUI {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new TestGUI();
     }
 
-    public TestGUI() {
+    public TestGUI() throws IOException {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("TestGUI");
         frame.setPreferredSize(new Dimension(800, 600));
-        frame.add(new TestMainPanel(1));
+        frame.getContentPane().add(new JPanelWithBackground("/home/ola/final_project/pictures/obrazek_piwnica.png"), BorderLayout.CENTER);
+        //frame.add(new TestMainPanel(1));
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public class JPanelWithBackground extends JPanel {
+
+        private Image backgroundImage;
+
+        // Some code to initialize the background image.
+        // Here, we use the constructor to load the image. This
+        // can vary depending on the use case of the panel.
+        public JPanelWithBackground(String fileName) throws IOException {
+            backgroundImage = ImageIO.read(new File(fileName));
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Draw the background image.
+            g.drawImage(backgroundImage, 0, 0, this);
+        }
     }
 
     class TestMainPanel extends JPanel {
