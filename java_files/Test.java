bc69a2819aa1496855484ec7a2046034a7ce763d
radzsza <aleksandraradziukiewicz@gmail.com>
2025-05-23 18:04:31 +0200

    Test.java update

--- java_files/Test.java
+++ java_files/Test.java
@@ -29,6 +29,9 @@ public class Test {
         public MyJButton(String text) {
             super();
             setText(text);
+            addActionListener( e-> {
+                // jeszcze nie wiem
+            });
         }
     }
 
@@ -38,48 +41,45 @@ public class Test {
             setPreferredSize(new Dimension(500, 500)); // making it bigger size
             setLayout(new GridBagLayout()); // just set the layout manager
             GridBagConstraints gbc = new GridBagConstraints(); // kinda makes the layout working
-            // setting layout size
-            gbc.gridwidth = 3;
-            gbc.gridheight = 3;
+
             //gbc.fill = GridBagConstraints.HORIZONTAL;
 
             JLabel label = new JLabel("Hi");
             gbc.gridx = 1;
             gbc.gridy = 1;
-            gbc.weightx = 0.5;
-            gbc.weighty = 0.5;
-            gbc.anchor = GridBagConstraints.CENTER;
+            gbc.weightx = 1;
+            gbc.weighty = 1;
+            //gbc.anchor = GridBagConstraints.CENTER;
             add(label, gbc);
 
             JButton button = new JButton("LU");
             gbc.gridx = 0;
             gbc.gridy = 0;
-            gbc.weighty = 0;
-            gbc.weightx = 0;
             gbc.anchor = GridBagConstraints.FIRST_LINE_START;
             add(button, gbc); // left upper corner
 
-            gbc.gridx = 3;
+            gbc.gridx = 2;
             gbc.gridy = 0;
-            gbc.weighty = 0;
             gbc.weightx = 1;
             gbc.anchor = GridBagConstraints.FIRST_LINE_END;
             button = new JButton("RU");
             add(button, gbc); // right upper corner
 
             gbc.gridx = 0;
-            gbc.gridy = 3;
+            gbc.gridy = 2;
             gbc.weighty = 1;
-            gbc.weightx = 0;
+            gbc.ipady = 40;
+            gbc.ipadx = 40;
             gbc.anchor = GridBagConstraints.LAST_LINE_START;
             button = new JButton("LB");
             add(button, gbc); // left bottom corner
 
-            gbc.gridx = 3;
-            gbc.gridy = 3;
+            gbc.gridx = 2;
+            gbc.gridy = 2;
             gbc.weighty = 1;
             gbc.weightx = 1;
-            // gbc.ipady = 40;
+            gbc.ipady = 40;
+            gbc.ipadx = 40;
             gbc.anchor = GridBagConstraints.LAST_LINE_END;
             button = new JButton("RB");
             add(button, gbc); // right bottom corner
---
+++ java_files/TestGUI.java
@@ -0,0 +1,4 @@
+package java_files;
+
+public class TestGUI {
+}
